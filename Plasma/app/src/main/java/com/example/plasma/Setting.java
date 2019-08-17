09319622e2d999cfc0f27747bddd88e815e0a25a
Afroza Sharmin Urmi <50110596+Afroza-Sharmin-Urmi@users.noreply.github.com>
2019-08-17 22:08:08 +0600

    Add files via upload

---
+++ Plasma/app/src/main/java/com/example/plasma/Setting.java
@@ -0,0 +1,17 @@
+package com.example.plasma.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.plasma.R;
+
+public class Setting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+    }
+}
---
+++ Plasma/app/src/main/java/com/example/plasma/User.java
@@ -0,0 +1,20 @@
+package com.example.plasma.test;
+
+public class  User {
+    public String Name, Password, Blood_group, Gender, Number, Email, Address;
+
+
+
+
+    public User(String name, String password, String blood_group, String gender, String number, String email, String address) {
+        this.Name = name;
+        Password = password;
+        Blood_group = blood_group;
+        Gender = gender;
+        Number = number;
+        Email = email;
+        Address = address;
+    }
+}
+
+
